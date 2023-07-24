0fc0a044abf95e3f83530ca46558d65b06d032be
erfan-zolghadri <ezolghadri98@gmail.com>
2023-07-24 14:27:38 +0330

    Make API documentation

--- app/config/settings.py
+++ app/config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "drf_spectacular",
     "django_filters",
     "accounts",
     "movies",
@@ -146,6 +147,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
